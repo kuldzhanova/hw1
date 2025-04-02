500bc2c4716d93a4c69d3d3f049ff7681ee469bf
user <user@MacBook-Air-user.local>
2025-04-02 21:00:16 +0600

    first commit

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,31 @@
-// MOVE BLOCK HW 1.2
+//GMAIL CHECKER
+const gmailInput = document.querySelector("#gmail_input");
+const gmailButton = document.querySelector("#gmail_button");
+const gmailResult = document.querySelector("#gmail_result");
 
-const parentBlock = document.querySelector('.parent_block');
-const childBlock = document.querySelector('.child_block');
+const regExp = /^\w+@gmail\.com$/
 
-let positionX = 0
-let positionY = 0
-
-const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
-const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
-
-const moveBlock = () => {
-    if (positionX < offWidth) positionX++
-    if (positionX >= offWidth && positionY < offHeight) positionY++
-    childBlock.style.left = `${positionX}px`
-    childBlock.style.top = `${positionY}px`
-    requestAnimationFrame(moveBlock)
+gmailButton.onclick = () => {
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML = "OK";
+        gmailResult.style.color = "green"
+    } else {
+        gmailResult.innerHTML =  'ERROR';
+        gmailResult.style.color = "red"
+    }
 }
 
-moveBlock();
+// part 2
+const block = document.querySelector(".child_block");
+block.style.left = '0px';
+let move = 0;
+const count = () => {
+    move++;
+    block.style.left = move + 'px';
+
+    if (move < 448) {
+        requestAnimationFrame(count)
+    }
+
+}
+count()
