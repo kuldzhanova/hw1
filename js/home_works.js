39866a65f9721963b661bcfed81badaed819c10f
user <user@MacBook-Air-user.local>
2025-04-06 20:13:49 +0600

    new commit

--- js/home_works.js
+++ js/home_works.js
@@ -15,17 +15,79 @@ gmailButton.onclick = () => {
     }
 }
 
-// part 2
-const block = document.querySelector(".child_block");
-block.style.left = '0px';
-let move = 0;
-const count = () => {
-    move++;
-    block.style.left = move + 'px';
+// MOVE BLOCK
 
-    if (move < 448) {
-        requestAnimationFrame(count)
+// const block = document.querySelector(".child_block");
+// block.style.left = '0px';
+// let move = 0;
+// const count = () => {
+//     move++;
+//     block.style.left = move + 'px';
+//
+//     if (move < 448) {
+//         requestAnimationFrame(count)
+//     }
+//
+// }
+// count()
+
+const childBlock = document.querySelector(".child_block");
+const parentBlock = document.querySelector(".parent_block");
+
+let positionX = 0, positionY = 0;
+const maxWidth = parentBlock.clientWidth - childBlock.clientWidth;
+const maxHeight = parentBlock.clientHeight - childBlock.clientHeight;
+
+const moveBlock = () => {
+    if (positionX < maxWidth && positionY === 0) {
+        positionX++;
+        childBlock.style.left = `${positionX}px`;
+    } else if (positionX >= maxWidth && positionY < maxHeight) {
+        positionY++;
+        childBlock.style.top = `${positionY}px`;
+    } else if (positionY >= maxHeight && positionX > 0) {
+        positionX--;
+        childBlock.style.left = `${positionX}px`;
+    } else if (positionX === 0 && positionY > 0) {
+        positionY--;
+        childBlock.style.top = `${positionY}px`;
     }
 
+    requestAnimationFrame(moveBlock);
+};
+
+moveBlock();
+
+// HOMEWORK 2
+const counter = document.querySelector("#seconds");
+const startBtn = document.querySelector("#start");
+const stopBtn = document.querySelector("#stop");
+const resetBtn = document.querySelector("#reset");
+let count = 0
+let interval = null;
+
+const newInterval = () => {
+    counter.innerHTML = count;
+};
+newInterval();
+
+startBtn.onclick = () => {
+    if (interval === null) {
+        interval = setInterval(() => {
+            count++;
+            newInterval(); // обновляем экран каждую секунду
+        }, 1000);
+    }
 }
-count()
+
+stopBtn.onclick = () => {
+    clearInterval(interval);
+    interval = null;
+}
+
+resetBtn.onclick = () => {
+    clearInterval(interval);
+    interval = null;
+    count = 0
+    newInterval();
+}
